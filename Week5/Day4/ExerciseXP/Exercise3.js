0c6829121d6744ed9ae85e83d7aa87f695aeaacc
credactor <175741800+credactor@users.noreply.github.com>
2024-09-04 11:17:28 +0300

    Week5 Day4 Exercises & Challenge

---
+++ Week5/Day4/ExerciseXP/Exercise3.js
@@ -0,0 +1,31 @@
+// Convert this JS object into a JSON object. What happens to the nested objects ?
+// Convert and pretty print this JS object into a JSON object. Hint : Check out the JSON lesson on the platform.
+// Use your web inspector to add a breakpoint. Check the values of the JSON object in the debugger.
+
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+}
+j1 = JSON.stringify(marioGame);
+console.log(j1); // Nested objects converted too.
+j2 = JSON.stringify(marioGame, null, 2);
+console.log(j2);
